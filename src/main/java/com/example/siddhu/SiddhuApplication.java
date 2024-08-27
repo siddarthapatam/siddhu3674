359bd36195509c9da312b011d5d49e0dac0a3c09
siddartha <siddarthapatam@gmail.com>
2024-08-27 11:02:39 +0530

    code

---
+++ src/main/java/com/example/siddhu/SiddhuApplication.java
@@ -0,0 +1,13 @@
+package com.example.siddhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiddhuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiddhuApplication.class, args);
+	}
+
+}
